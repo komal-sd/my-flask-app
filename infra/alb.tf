0cb5dc62ad1e175f674c870ffd215e443edd97e2
Komal <komalbhu007@gmail.com>
2026-03-27 16:18:33 +0530

    Add alb.tf: Load balancer with health checks

---
+++ infra/alb.tf
@@ -0,0 +1,46 @@
+# ================== APPLICATION LOAD BALANCER =======================
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+# =================== TARGET GROUP ===================================
+resource "aws_lb_target_group" "main" {
+  name        = "${var.project_name}-tg"
+  port        = var.app_port
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    path                = "/health"
+    matcher             = "200"
+  }
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
+}
+
+# ================= ALB LISTENER ========================   
+# listener for ALB for incoming traffic
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
